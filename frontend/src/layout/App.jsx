36eac7a7e30a2f8920a8b9a6caf56d808098426e
Michael <mgainey@hawaii.edu>
2022-05-03 22:07:12 -1000

    added router and starter routes for navbar

--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import '../styles/App.css';
-import Button from 'react-bootstrap/Button';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Profile from '../pages/Profile';
+import Error from '../pages/Error';
 
 const App = () => {
   return (
-    <div id='landing'>
-      <p>Hello World!</p>
-      <Button>click me</Button>
-
-    </div>
+    <Router>
+      {/* put navbar here */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 };
 
